a5bc1c1d103d21598a35586a33f554281fcc7924
Deepali2 <hdgarg@gmail.com>
2018-06-27 21:09:52 -0700

    Complete until click event handler on video title

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 // TODO: Render the `App` component to the DOM
+ReactDOM.render(<App videos={window.exampleVideoData}/>, document.getElementById('app'));
 
+
+// ReactDOM.render(<VideoList />, $(".col-md-5")[0]);
+
+// ReactDOM.render(<VideoListEntry />, document.getElementById("app"));
+
+// ReactDOM.render(<VideoPlayer />, $(".col-md-7")[0]);
+
+// ReactDOM.render(<Search />, $(".col-md-6")[0]);
